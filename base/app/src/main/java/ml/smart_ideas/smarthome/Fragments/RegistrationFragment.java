608db7426790567c774f21cf35c1e62eed6e28cb
mtudjan <mtudjan@foi.hr>
2015-11-13 22:15:18 +0100

    Dodana klasa za manipulaciju Registracijom -nema funkcionalnosti

---
+++ base/app/src/main/java/ml/smart_ideas/smarthome/Fragments/RegistrationFragment.java
@@ -0,0 +1,15 @@
+package ml.smart_ideas.smarthome.Fragments;
+
+
+
+import android.support.v4.app.Fragment;
+
+
+/**
+ * Created by mario on 10.11.2015..
+ */
+public class RegistrationFragment extends Fragment {
+
+
+
+}
