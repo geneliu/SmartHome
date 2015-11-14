5736da9fbb643792a969db445a1c193d57a4a145
Dubravko <dkisic2@foi.hr>
2015-11-14 10:53:17 +0100

    Modificiran registration_fragment.xml

--- base/app/src/main/java/ml/smart_ideas/smarthome/Fragments/RegistrationFragment.java
+++ base/app/src/main/java/ml/smart_ideas/smarthome/Fragments/RegistrationFragment.java
@@ -2,28 +2,16 @@ package ml.smart_ideas.smarthome.Fragments;
 
 
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.text.method.PasswordTransformationMethod;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 
-import com.google.gson.Gson;
-
-import ml.smart_ideas.smarthome.HomeScreenActivity;
 import ml.smart_ideas.smarthome.R;
-import ml.smart_ideas.smarthome.core.Globals;
-import ml.smart_ideas.smarthome.db.Korisnik;
-import ml.smart_ideas.smarthome.ws.model.Odgovor;
-import ml.smart_ideas.smarthome.ws.rest.RestClient;
-import retrofit.Call;
-import retrofit.Callback;
-import retrofit.Response;
 
 
 /**
@@ -44,7 +32,7 @@ public class RegistrationFragment extends Fragment {
                              Bundle savedInstanceState) {
 
         View viewInflater = inflater.inflate(R.layout.registration_fragment, container, false);
-        getActivity().setTitle("SmartHome registracija");
+        //getActivity().setTitle("SmartHome registracija");
 
         ETname = (EditText) viewInflater.findViewById(R.id.name);
         ETsurname = (EditText) viewInflater.findViewById(R.id.surname);
