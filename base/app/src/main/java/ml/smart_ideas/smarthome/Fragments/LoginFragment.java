ffd7cf4abbcd67dd4413a0b156db97e877ee76a7
mtudjan <mtudjan@foi.hr>
2015-11-14 14:41:54 +0100

    Dodana mogućnost promjene fragmenta putem evenata i odvojena je komunikacija sa serverom -dodan interface eventa -dodana upravljanje eventima na globals klasi - implementirana pretplata na event u InitialActivity klasi -koristi se spomenuta funkcionalnost u novoj klasi ServerCommunication u kojoj je i izvojen dio LoginFragmenta tako da se ostvari modularnost

--- base/app/src/main/java/ml/smart_ideas/smarthome/Fragments/LoginFragment.java
+++ base/app/src/main/java/ml/smart_ideas/smarthome/Fragments/LoginFragment.java
@@ -23,6 +23,7 @@ import ml.smart_ideas.smarthome.HomeScreenActivity;
 import ml.smart_ideas.smarthome.R;
 import ml.smart_ideas.smarthome.core.Globals;
 import ml.smart_ideas.smarthome.db.Korisnik;
+import ml.smart_ideas.smarthome.ws.connection.ServerCommunication;
 import ml.smart_ideas.smarthome.ws.model.Odgovor;
 import ml.smart_ideas.smarthome.ws.rest.RestClient;
 import retrofit.Call;
@@ -93,53 +94,9 @@ public class LoginFragment extends Fragment  {
 
 
     public void login() {
-        String user = ETusername.getText().toString();
-        String pw = ETpassword.getText().toString();
+        String username = ETusername.getText().toString();
+        String password = ETpassword.getText().toString();
 
-        RestClient.loginInterface service = RestClient.getClient();
-        //     Call<Odgovor> call = service.postWithJson(new LoginInfo("test1", "test1"));
-        Call<Odgovor> call = service.postWithFormParams(user, pw);
-        call.enqueue(new Callback<Odgovor>() {
-            @Override
-            public void onResponse(Response<Odgovor> response) {
-
-                Log.d("MainActivity", "Status Code = " + response.code());
-                if (response.isSuccess()) {
-                    // request successful (status code 200, 201)
-                    Odgovor result = response.body();
-                    Log.d("MainActivity", "response = " + new Gson().toJson(result));
-
-                    String stringUsername = result.getUsername();
-                    Log.d("MainActivity", "username = " + stringUsername);
-                    if (stringUsername != null) {
-                        String message = "";
-
-                        Globals.getInstance().setKorisnik(new Korisnik(stringUsername,"Ime","Prezime"));
-
-                        message = "Pozdrav " + stringUsername;
-                        Intent intent = new Intent(getContext(), HomeScreenActivity.class);
-                        intent.putExtra(EXTRA_MESSAGE, message);
-                        startActivity(intent);
-                    } else {
-                        String message = "";
-                        message = "Krivi podaci";
-                        Intent intent = new Intent(getContext(), HomeScreenActivity.class);
-                        intent.putExtra(EXTRA_MESSAGE, message);
-                        startActivity(intent);
-                    }
-
-
-                } else {
-                    // response received but request not successful (like 400,401,403 etc)
-                    //Handle errors
-
-                }
-            }
-
-            @Override
-            public void onFailure(Throwable t) {
-
-            }
-        });
+        ServerCommunication.getInstance().loginToServer(username,password);
     }
 }
