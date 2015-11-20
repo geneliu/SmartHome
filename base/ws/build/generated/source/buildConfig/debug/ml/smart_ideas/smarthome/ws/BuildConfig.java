15638928cd946a96d56a157f6a80e967cb65ac82
1susec <isusec@foi.hr>
2015-11-20 20:17:54 +0100

    Merge

---
+++ base/ws/build/generated/source/buildConfig/debug/ml/smart_ideas/smarthome/ws/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package ml.smart_ideas.smarthome.ws;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "ml.smart_ideas.smarthome.ws";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
